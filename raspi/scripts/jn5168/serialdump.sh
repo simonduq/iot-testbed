0949ff233bff751c282b057112a16fc1978e70be
Simon Duquennoy <simonduq@sics.se>
2015-02-04 09:39:24 +0100

    Fix bug in serialdump script

--- raspi/scripts/jn5168/serialdump.sh
+++ raspi/scripts/jn5168/serialdump.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-#/home/user/testbed-tools/usb-hub-off.sh
-#/home/user/testbed-tools/usb-hub-on.sh
 log_path=$1
 tty_path=`ls /dev/serial/by-id/*NXP_JN5168_USB_Dongle*`
 nohup ~/scripts/jn5168/contiki-serialdump -b1000000 $tty_path | ~/scripts/jn5168/contiki-timestamp > $log_path & > /dev/null 2> /dev/null
 sleep 1
-ps | grep " $! "
+ps | grep "$! "
 exit $?
