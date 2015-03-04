f019a266f5f642f8306b57e95dffe1ab71d5e618
Simon Duquennoy <simonduq@sics.se>
2015-03-04 13:50:29 +0100

    Add uptime to status command

--- raspi/scripts/check-date.sh
+++ raspi/scripts/check-date.sh
@@ -1,2 +1,2 @@
 now_ns=`date +%s%N`
-echo Date: `date`. Time offset with server \(incl. pssh latency\): $((($1 - $now_ns)/1000000))ms
+echo Uptime: `uptime | cut -d, -f1-2`. Time offset with server \(incl. pssh latency\): $((($1 - $now_ns)/1000000))ms
