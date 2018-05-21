10a3acd3a2b4860afdc84a478ef50f1236c4e779
Simon Duquennoy <simonduq@sics.se>
2018-05-21 18:27:18 +0200

    cng-bot enable parsing

--- server/scripts/contiki-ng-bot/cng-bot-post-process.py
+++ server/scripts/contiki-ng-bot/cng-bot-post-process.py
@@ -49,8 +49,8 @@ def main():
     outFile.write("commit: %s\n" %(taskData["commit"]))
 
     # Output relevant metrics
-    #ret = subprocess.check_output(["python3", os.path.join(dir, "parse.py"), dir])
-    #outFile.write(ret.decode("utf-8") )
+    ret = subprocess.check_output(["python3", os.path.join(dir, "parse.py"), dir])
+    outFile.write(ret.decode("utf-8") )
 
     outFile.write("---\n")
     outFile.write("\n{% include run.md %}\n")
