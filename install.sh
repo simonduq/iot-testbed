e03e8a65ef56c60ff9a224562e626f178ce1ded1
Simon Duquennoy <simonduq@sics.se>
2018-05-22 10:00:48 +0200

    Work on install.sh script

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-cp -R server/scripts/* /usr/testbed/scripts/
-parallel-rsync -h /usr/testbed/scripts/all-hosts -l user -azv --recursive raspi/scripts /home/user
+rsync -azv --delete server/scripts /usr/testbed/
+parallel-rsync -h /usr/testbed/scripts/all-hosts -l user -azv raspi/scripts /home/user
